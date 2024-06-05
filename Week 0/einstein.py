f6cf0275c08d62c4c6196563c86f917fcbb9f9eb
Matt <142843631+cheksumhov@users.noreply.github.com>
2024-06-05 13:23:05 -0400

    Create einstein.py

---
+++ Week 0/einstein.py
@@ -0,0 +1,8 @@
+# Ask user for input and store it in a variable named mass
+mass = int(input("Enter a mass:" ))
+
+# Perform calucation to find joules and then store the result in a variable named joules
+joules = mass * 300000000 ** 2
+
+# Print the joules variable
+print(joules)
