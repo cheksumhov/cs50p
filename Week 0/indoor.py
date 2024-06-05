445ea41422de88b68c471e9dcfae0979faca20a4
Matt <142843631+cheksumhov@users.noreply.github.com>
2024-06-05 12:23:36 -0400

    Update and rename indoor.py to indoor.py

---
+++ Week 0/indoor.py
@@ -0,0 +1,5 @@
+# Prompt user for the input and store it in a variable
+usertext = input("Enter something in all caps: ")
+
+# Print the variable and format it using the .lower method
+print(usertext.lower())
