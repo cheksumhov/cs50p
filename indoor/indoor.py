b7407eb4e010e00b42eeba2ecf8dc21326625ae8
Matt <142843631+cheksumhov@users.noreply.github.com>
2024-06-05 10:49:51 -0400

    Create indoor.py

---
+++ indoor/indoor.py
@@ -0,0 +1,9 @@
+# Assignment: In a file called indoor.py, implement a program in Python that prompts the user for input and then outputs that same input in lowercase.
+# Punctuation and whitespace should be outputted unchanged. You’re welcome, but not required, to prompt the user explicitly, as by passing a str of your own as an argument to input.
+
+
+# Prompt user for the input and store it in a variable
+usertext = input("Enter something in all caps: ")
+
+# Print the variable and format it using the .lower method
+print(usertext.lower())
