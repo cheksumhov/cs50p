771ccda6633c76bf5c4a06571f948e52fba392b3
Matt <142843631+cheksumhov@users.noreply.github.com>
2024-06-05 13:20:21 -0400

    Create playback.py

---
+++ Week 0/playback.py
@@ -0,0 +1,5 @@
+# Prompt user for a complete sentence
+usertext = input("Enter a complete sentence: ")
+
+# Replace spaces with elipses "..." and then print the result
+print(usertext.replace(" ", "..."))
